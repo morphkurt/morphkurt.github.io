8019353f4cce83f74ba50dab05836b04ec833343
Damitha Gunawardena <damitha.gunawardena+github@gmail.com>
2016-11-03 10:01:29 +1100

    Update app.js

--- serveradmin/js/app.js
+++ serveradmin/js/app.js
@@ -20,7 +20,7 @@ $( document ).ready(function() {
 			    	|| (servers.servers[i].mgt_ip.search(text) > -1))
 			    {
 				    html += '<tr><td><p class="text-left">'+servers.servers[i].servername+'</p>'+ 
-                    '</td><td><i onclick="location.href=\'ssh://'+servers.servers[i].mgt_ip+':'+servers.servers[i].ssh_port+'\'" class="fa fa-terminal fa-2x" aria-hidden="true"></i></a></td>' +
+                    '</td><td><i target="_blank" onclick="location.href=\'ssh://'+servers.servers[i].mgt_ip+':'+servers.servers[i].ssh_port+'\'" class="fa fa-terminal fa-2x" aria-hidden="true"></i></a></td>' +
                     '</td><td><i onclick="location.href=\'http://'+servers.servers[i].mgt_ip+':'+servers.servers[i].web_port+'\'" class="fa fa-chrome fa-2x" aria-hidden="true"></i></td>' +
                     '</td><td><i onclick="location.href=\'https://'+servers.servers[i].ilo_ip+'\'" class="fa fa-chrome fa-2x" aria-hidden="true"></i></td>' +
                     '</td><td>' + servers.servers[i].environment + '</td>' +
