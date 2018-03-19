8f0adc515a1977d850f6c309f2af707820357efa
Damitha Gunawardena <damitha.gunawardena+github@gmail.com>
2018-03-19 21:45:46 +1100

    Update app.js

--- rateSwitcher/js/app.js
+++ rateSwitcher/js/app.js
@@ -167,11 +167,11 @@ function handleFileSelect(evt) {
          config=JSON.parse(JsonObj);
          $('#resultstable').empty();
          var html='';  
-         for (i = 0; i < config.assets.length; i++) { 
-      	   html += '<tr><td><p class="text-left">'+config.assets[i].channel_name+'</p></td>' +
-      	   '<td><p class="text-left">'+config.assets[i].embed_code+'</p></td>'+
-           '<td><button onclick="ImplementChange(\''+config.assets[i].embed_code+'\');" type="button" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-edit" aria-hidden="true"></span></button></td>'+
-           '<td><button onclick="infoclick(\''+config.assets[i].embed_code+'\');" type="button" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-info-sign" aria-hidden="true"></span></button></td>'+
+         for (i = 0; i < config.channels.length; i++) { 
+      	   html += '<tr><td><p class="text-left">'+config.channels[i].channel_name+'</p></td>' +
+      	   '<td><p class="text-left">'+config.channels[i].embed_code+'</p></td>'+
+           '<td><button onclick="ImplementChange(\''+config.channels[i].embed_code+'\');" type="button" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-edit" aria-hidden="true"></span></button></td>'+
+           '<td><button onclick="infoclick(\''+config.channels[i].embed_code+'\');" type="button" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-info-sign" aria-hidden="true"></span></button></td>'+
       	   '</tr>'
      	  }
    
