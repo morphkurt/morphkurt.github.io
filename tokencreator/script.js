a434c0368af370d1386cd335f66b62063d50e481
Damitha Gunawardena <damitha.gunawardena+github@gmail.com>
2018-11-23 14:20:17 +1100

    Update script.js

--- tokencreator/script.js
+++ tokencreator/script.js
@@ -1,11 +1,15 @@
-
- // Initialize Firebase
-  var config = {
+// Initialize Firebase
+var config = {
     apiKey: "AIzaSyAznzsmO8oDgp70bp0a6KLfRfxJbBSJrCg",
     authDomain: "test-d7a2d.firebaseapp.com",
     databaseURL: "https://test-d7a2d.firebaseio.com",
     projectId: "test-d7a2d",
     storageBucket: "test-d7a2d.appspot.com",
     messagingSenderId: "1047309579449"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
+
+var ref = firebase.database().ref();                           
+ref.on("value", function(snapshot){
+    output.innerHTML = JSON.stringify(snapshot.val(), null, 2);
+});
