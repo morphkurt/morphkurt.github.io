14fea8b7ac8dc449854ffeb42ac3ddd92c05c9f4
Damitha Gunawardena <damitha.gunawardena+github@gmail.com>
2018-11-23 14:19:05 +1100

    Create script.js

---
+++ tokencreator/script.js
@@ -0,0 +1,11 @@
+
+ // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAznzsmO8oDgp70bp0a6KLfRfxJbBSJrCg",
+    authDomain: "test-d7a2d.firebaseapp.com",
+    databaseURL: "https://test-d7a2d.firebaseio.com",
+    projectId: "test-d7a2d",
+    storageBucket: "test-d7a2d.appspot.com",
+    messagingSenderId: "1047309579449"
+  };
+  firebase.initializeApp(config);
