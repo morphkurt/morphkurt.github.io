138b4b464f4474ae54ff5b9ecc7940b3ffca922d
Damitha Gunawardena <damitha.gunawardena+github@gmail.com>
2019-02-04 15:39:21 +1100

    Create app.js

---
+++ garagedoor/js/app.js
@@ -0,0 +1,17 @@
+function pressRemote(){
+	$.ajax({
+        type: "GET",
+        url: "http://192.168.88.105/press,
+        dataType: "text",
+        success: function(data) { 
+     	}
+	});
+}
+
+( document ).ready(function() {
+
+  $("input[@name='options']").change(function(){
+       pressRemote();
+  });
+
+});
