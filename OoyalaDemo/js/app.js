f0598b1e6ee5cf61845c784748927d9fe4183e87
Damitha Gunawardena <damitha.gunawardena+github@gmail.com>
2019-02-12 10:55:47 +1100

    Update app.js

--- OoyalaDemo/js/app.js
+++ OoyalaDemo/js/app.js
@@ -4,7 +4,7 @@ var pCode="";
 $( document ).ready(function() {
   embedCode=getName("ec");
   pCode=getName("pc");
-  $('#ooyala-player-dev').text("<p><iframe id=\"ooyala-player-frame\" width=\"550\" height=\"310\" src=\"https://player.ooyala.com/static/v4/production/skin-plugin/iframe.html?ec="+embedCode+"&pbid=4d619a02ee0644649726510a6dce17f7&pcode="+pCode+"&autoplay=falseframeborder=\" allowfullscreen=\"0=\"></iframe></p>");
+  $('#ooyala-player-div').text("<p><iframe id=\"ooyala-player-frame\" width=\"550\" height=\"310\" src=\"https://player.ooyala.com/static/v4/production/skin-plugin/iframe.html?ec="+embedCode+"&pbid=4d619a02ee0644649726510a6dce17f7&pcode="+pCode+"&autoplay=falseframeborder=\" allowfullscreen=\"0=\"></iframe></p>");
   
 });
 
